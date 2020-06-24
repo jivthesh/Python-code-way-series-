e1c6a09176451b21ed4843306c1802b41ea347a9
Jivthesh M <jivtheshm@gmail.com>
2020-06-24 21:28:33 +0530

    Day2task

--- Pythontask2/sets.py
+++ Pythontask2/sets.py
@@ -1,43 +1,57 @@
-# creating a set:
+# working with sets
 
-fruits = {'apple', 'banana', 'grapes', 'cherry'}
-print("The set of fruits : ", fruits)
+# creating a set
+myset = {1,2,3,4,5,6,7,8,9}
 
-# using add() method
-fruits.add("avacado")  # using add() method
-print("fruits set after adding", fruits)
 
-# using update() method
-fruits.update(["berries", "Mango"])  # using update() method
-print("set after updation: ", fruits)
+# maximum element in the set
+setmaximum = max(myset)					
+print(setmaximum)
 
-# using length() method
-length = len(fruits)
-print("The length of the fruits set is: ", length)
 
-# using remove() method
-fruits.remove("avacado")
-print("The fruits set after removing ", fruits)
+# minimum element in the set
+setminimum = min(myset)
+print(setminimum)
 
-# remove the specific item/element
-# discard/remove both can be used to remove item
-# if the item which is to be remove is not present then remove will raise an error ,discard will not.
-fruits.discard("cherry")
-print("The fruits set after discarding ", fruits)
 
-# using pop() method
-fruits.pop()
-print("The fruits set after performing a pop opearation: ", fruits)
+# looping in sets
+for x in myset:
+    print(x)
 
-# using clear() method
-fruits.clear()
-print("The set fruits after using clear method: ", fruits)
+# length of the set
+setlength = len(myset)
+print(setlength)
 
-# Using the union function to return the union of two sets
-fruits1 = {"Passion Fruit"}
-mainSet = fruits.union(fruits1)
-print("The set fruits after using union function method: ",mainSet)
+					
+# adding element in the list 
+myset.add(5)
+print(myset)
 
-# Using the difference function to return the union of two sets
-mainSet2 = mainSet.union(fruits)
-print("The set fruits after using clear method: ",mainSet2)
+
+# adding multiple elements in the list
+firstupdate = myset.update([34,67,65,87])
+print(myset)
+
+
+# copying the set
+nextset = myset.copy()
+print(nextset)
+
+
+
+nextset = {5,6,7,8,9}
+nextset_1 = {2,3,4,5,6}
+
+# intersection of the elements in the set
+mysetintersection = myset.intersection(nextset , nextset_1)
+print(mysetintersection)
+
+
+#difference of the elements in the set
+mysetdifference = myset.difference(nextset)	
+print(mysetdifference)
+
+
+# resulting the whole set
+unionSet = myset.union(nextset,nextset_1)
+print(unionSet)
