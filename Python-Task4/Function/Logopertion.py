c374fcb4679d38a4c665903aada1b93b1cedc42d
jivthesh <jivtheshm@gmail.com>
2020-06-27 12:08:55 -0700

    Day4task

---
+++ Python-Task4/Function/Logopertion.py
@@ -0,0 +1,19 @@
+# function for And operator
+
+def Andoperator(x, y):
+    if x > 0 and y > 0:
+        print('Both x and y are positive numbers')
+
+# function for Or operator
+
+def Oroperator(x, y):
+    if x > 0 or y > 0:
+        print('At least one of x and y is positive number')
+
+#function for not operator
+
+def Notoperator(x, y):
+    if not x % 2 == 0 or y % 2 == 0:
+        print("no is divisible by 2")
+
+
