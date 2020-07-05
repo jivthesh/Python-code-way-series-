f35ae5d0567f7c1e422ae896b239c686375188c0
Jivthesh M R <jivtheshm@gmail.com>
2020-07-05 09:41:32 -0700

    Day 6 Task

--- Python-Task6/Q6.py
+++ Python-Task6/Q6.py
@@ -1,14 +1,18 @@
+#Q6
 # function is declared
+
 def findType(n):
     sum = 0
 
     # addition of all divisors
+    
     for i in range(1, n):
         if (n % i) == 0:
             sum = sum + i
     print("the sum of divisors is:", sum)
 
     # checking the condition
+    
     if sum < n:
         z = 1
     elif sum > n:
@@ -18,6 +22,7 @@ def findType(n):
     return (z)
 
 # calling function findType
+
 n = int(input("enter the number you wanted to check is perfect or not: "))
 result = findType(n)
 if (result == 1):
