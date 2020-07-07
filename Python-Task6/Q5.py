17196ba5a9b0c33bd9a779b8f98e9a633b600968
Jivthesh M R <jivtheshm@gmail.com>
2020-07-06 23:04:49 -0700

    Update Q5.py

--- Python-Task6/Q5.py
+++ Python-Task6/Q5.py
@@ -1,10 +1,15 @@
 #Q5
 
 #input from the user
+
 test_string = input("Enter a string:")
+
 print("Original string:", test_string)
+
 res = len(test_string.split())
+
 # printing result
+
 print("The number of words in string are : ", str(res))
 
 
